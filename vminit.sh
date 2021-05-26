68ddd7fe60625346cfb21ad18ad625594cab4f2d
Dragoonix2000 <47521281+Dragoonix2000@users.noreply.github.com>
2021-05-26 17:31:55 +0200

    Update vminit.sh

--- vminit.sh
+++ vminit.sh
@@ -21,28 +21,31 @@ CLIQR_HOME=/usr/local/osmosix
 
 ## Create new user
 #sudo agentSendLogMessage "Creating new user..."
-# if [ -d /home/$MY_USER ]; then
-	# sudo agentSendLogMessage "$MY_USER already exists."
-# else
-	# sudo adduser -m -d /home/$MY_USER $MY_USER
-	# sudo mkdir /home/$MY_USER/.ssh
-	# sudo chown $MY_USER:$MY_USER /home/$MY_USER/.ssh
-	# sudo chmod 700 /home/$MY_USER/.ssh
-	# sudo touch /home/$MY_USER/.ssh/authorized_keys
-	# sudo chown $MY_USER:$MY_USER /home/$MY_USER/.ssh/authorized_keys
-	# sudo chmod 600 /home/$MY_USER/.ssh/authorized_keys
-	# sudo agentSendLogMessage "New user $MY_USER created."
-# fi
+if [ -d /home/$MY_USER ]; then
+	sudo agentSendLogMessage "$MY_USER already exists."
+else
+	sudo useradd -m -d /home/$MY_USER $MY_USER
+	sudo mkdir /home/$MY_USER/.ssh
+	sudo chown $MY_USER:$MY_USER /home/$MY_USER/.ssh
+	sudo chmod 700 /home/$MY_USER/.ssh
+	sudo touch /home/$MY_USER/.ssh/authorized_keys
+	sudo chown $MY_USER:$MY_USER /home/$MY_USER/.ssh/authorized_keys
+	sudo chmod 600 /home/$MY_USER/.ssh/authorized_keys
+	sudo agentSendLogMessage "New user $MY_USER created."
+fi
+
+#LOG VARIABELS
+touch /home/cliqruser/test.txt
+echo $MY_KEY >> /home/cliqruser/test.txt
+echo "##user##" >> /home/cliqruser/test.txt
+echo $MY_USER >> /home/cliqruser/test.txt
 
 ## Add user to sudoers
 #sudo agentSendLogMessage "Adding cliqruser and $MY_USER users to sudoers..."
-#sudo usermod -aG wheel $MY_USER
-#sudo usermod -aG wheel cliqruser
-#sudo -i bash -c "echo \"$MY_USER  ALL= NOPASSWD: ALL\" >> /etc/sudoers"
-touch /home/cliqruser/test.txt
-echo $MY_KEY > /home/cliqruser/test.txt
-echo "##user##" > /home/cliqruser/test.txt
-echo $MY_USER > /home/cliqruser/test.txt
+sudo usermod -aG wheel $MY_USER
+sudo usermod -aG wheel cliqruser
+sudo -i bash -c "echo \"$MY_USER  ALL= NOPASSWD: ALL\" >> /etc/sudoers"
+
 
 ## Insert keys for new user
 sudo agentSendLogMessage "Adding a new key to cliqruser and $MY_USER authorized_keys..."
