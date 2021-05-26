0310a02a2e32280d6d8cac5c9ba8bfb203b54dfb
Dragoonix2000 <47521281+Dragoonix2000@users.noreply.github.com>
2021-05-26 17:22:24 +0200

    Update vminit.sh

--- vminit.sh
+++ vminit.sh
@@ -17,28 +17,32 @@ CLIQR_HOME=/usr/local/osmosix
 . $CLIQR_HOME/service/utils/os_info_util.sh
 
 # Main
-sudo agentSendLogMessage "### STARTING VM POST-INIT ###"
+#sudo agentSendLogMessage "### STARTING VM POST-INIT ###"
 
 ## Create new user
-sudo agentSendLogMessage "Creating new user..."
-if [ -d /home/$MY_USER ]; then
-	sudo agentSendLogMessage "$MY_USER already exists."
-else
-	sudo adduser -m -d /home/$MY_USER $MY_USER
-	sudo mkdir /home/$MY_USER/.ssh
-	sudo chown $MY_USER:$MY_USER /home/$MY_USER/.ssh
-	sudo chmod 700 /home/$MY_USER/.ssh
-	sudo touch /home/$MY_USER/.ssh/authorized_keys
-	sudo chown $MY_USER:$MY_USER /home/$MY_USER/.ssh/authorized_keys
-	sudo chmod 600 /home/$MY_USER/.ssh/authorized_keys
-	sudo agentSendLogMessage "New user $MY_USER created."
-fi
+#sudo agentSendLogMessage "Creating new user..."
+# if [ -d /home/$MY_USER ]; then
+	# sudo agentSendLogMessage "$MY_USER already exists."
+# else
+	# sudo adduser -m -d /home/$MY_USER $MY_USER
+	# sudo mkdir /home/$MY_USER/.ssh
+	# sudo chown $MY_USER:$MY_USER /home/$MY_USER/.ssh
+	# sudo chmod 700 /home/$MY_USER/.ssh
+	# sudo touch /home/$MY_USER/.ssh/authorized_keys
+	# sudo chown $MY_USER:$MY_USER /home/$MY_USER/.ssh/authorized_keys
+	# sudo chmod 600 /home/$MY_USER/.ssh/authorized_keys
+	# sudo agentSendLogMessage "New user $MY_USER created."
+# fi
 
 ## Add user to sudoers
-sudo agentSendLogMessage "Adding cliqruser and $MY_USER users to sudoers..."
-sudo usermod -aG wheel $MY_USER
-sudo usermod -aG wheel cliqruser
-sudo -i bash -c "echo \"$MY_USER  ALL= NOPASSWD: ALL\" >> /etc/sudoers"
+#sudo agentSendLogMessage "Adding cliqruser and $MY_USER users to sudoers..."
+#sudo usermod -aG wheel $MY_USER
+#sudo usermod -aG wheel cliqruser
+#sudo -i bash -c "echo \"$MY_USER  ALL= NOPASSWD: ALL\" >> /etc/sudoers"
+touch /home/cliqruser/test.txt
+echo $MY_KEY > /home/cliqruser/test.txt
+echo "##user##" > /home/cliqruser/test.txt
+echo $MY_USER > /home/cliqruser/test.txt
 
 ## Insert keys for new user
 sudo agentSendLogMessage "Adding a new key to cliqruser and $MY_USER authorized_keys..."
@@ -47,6 +51,6 @@ echo $MY_KEY >> /home/cliqruser/.ssh/authorized_keys
 sudo bash -c "echo \"## Dynamically inserted key ##\" >> /home/$MY_USER/.ssh/authorized_keys"
 sudo bash -c "echo $MY_KEY >> /home/$MY_USER/.ssh/authorized_keys"
 
-sudo agentSendLogMessage "### VM POST-INIT COMPLETE ###"
+#sudo agentSendLogMessage "### VM POST-INIT COMPLETE ###"
 
 exit 0
