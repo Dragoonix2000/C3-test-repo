866f1d35eff808de24856c54992af356a477fb5c
Dragoonix2000 <47521281+Dragoonix2000@users.noreply.github.com>
2021-06-08 12:12:46 +0200

    update

--- nginx_webserver2/nginx_service2.sh
+++ nginx_webserver2/nginx_service2.sh
@@ -18,15 +18,14 @@ case $cmd in
 		sudo apt-get install -y unzip
 		;;
     deploy)
-
-		;;
-    configure)
 		wget $nginxAppFile
 		sudo unzip /opt/remoteFiles/nginxAppFile/$nginxAppZip -d /opt/remoteFiles/nginxAppFile/
 		sudo mkdir /etc/nginx/www
 		sudo cp /opt/remoteFiles/nginxAppFile/index.html /etc/nginx/www/index.html
 		sudo cp /opt/remoteFiles/nginxAppFile/nginx.conf /etc/nginx/nginx.conf
 		sudo systemctl restart nginx.service
+		;;
+    configure)
 		;;
     start)
 		;;
